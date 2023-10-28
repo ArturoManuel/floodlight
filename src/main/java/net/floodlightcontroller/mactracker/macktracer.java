67e4f6a2087d9a231dfa38936e19f0b1b54413d4
ArturoManuel <noriegaarturonoriega@gmail.com>
2023-10-28 16:27:45 -0500

    inicio

---
+++ src/main/java/net/floodlightcontroller/mactracker/macktracer.java
@@ -0,0 +1,93 @@
+package net.floodlightcontroller.mactracker;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.Ethernet;
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class macktracer implements IOFMessageListener, IFloodlightModule {
+
+    protected IFloodlightProviderService floodlightProvider;
+    protected Set<MacAddress> macAddresses;
+    protected static Logger logger;
+
+
+
+    @Override
+    public String getName() {
+        return macktracer.class.getSimpleName();
+    }
+
+
+    @Override
+    public boolean isCallbackOrderingPrereq(OFType type, String name) {
+        return false;
+    }
+
+    @Override
+    public boolean isCallbackOrderingPostreq(OFType type, String name) {
+        return false;
+    }
+
+    @Override
+    public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+        Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+        MacAddress sourceMAC = eth.getSourceMACAddress();
+
+        if (!macAddresses.contains(sourceMAC)) {
+            macAddresses.add(sourceMAC);
+            logger.info("MAC Address: {} seen on switch: {}", sourceMAC.toString(), sw.getId().toString());
+        }
+
+        return Command.CONTINUE;
+    }
+
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+        return null;
+    }
+
+    @Override
+    public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+        return null;
+    }
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+        Collection<Class<? extends IFloodlightService>> l = new ArrayList<>();
+        l.add(IFloodlightProviderService.class);
+        return l;
+    }
+
+
+    @Override
+    public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+        floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+        macAddresses = new ConcurrentSkipListSet<>();
+        logger = LoggerFactory.getLogger(macktracer.class);
+    }
+
+
+    @Override
+    public void startUp(FloodlightModuleContext context) {
+        floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+    }
+
+}
